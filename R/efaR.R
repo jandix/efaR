52b5025fe5c71060e445f08487065198bf6add11
tolpatsch <dix.jan@gmx.de>
2017-06-20 13:01:34 +0200

    update main function efaR

--- R/efaR.R
+++ R/efaR.R
@@ -2,8 +2,7 @@
 #'
 #' @param origin The origin of the journey. Either a name or a DHID Code. If a name is provided the next station is calculated based on the geocode function and the 'Haversine distance'.
 #' @param destination The destination of the journey. Either a name or a DHID Code. If a name is provided the next station is calculated based on the geocode function and the 'Haversine distance'.
-#' @param date The date of the journey.
-#' @param time The time of the journey.
+#' @param time The date and time of the journey.
 #' @param simplify Simplify determines whether the original xml result is returned or a simpler S3 object.
 #'
 #' @return Either the original xml result or an S3 object based on the simplify input. 
@@ -12,7 +11,6 @@
 
 efaR <- function (origin,
                   destination,
-                  date = Sys.Date(),
                   time = Sys.time(),
                   simplify = TRUE
 ) {
@@ -62,11 +60,21 @@ efaR <- function (origin,
     zHV$distance <- geosphere::distHaversine(c(coordinates$lon, coordinates$lat),
                                              cbind(zHV$Longitude, zHV$Latitude))
     zHV <- dplyr::arrange(zHV, distance)
-    origin <- list(name = zHV$Name[1], dhid = zHV$DHID[1])
+    destination <- list(name = zHV$Name[1], dhid = zHV$DHID[1])
+  } else {
+    if(any(stringr::str_detect(zHV$DHID, destination))){
+      destination <- list(name = zHV$Name[1], dhid = zHV$DHID[1])
+    } else {
+      stop(
+        sprintf(
+          "Origin DHID code is not in data set."
+        ),
+        call. = FALSE
+      )
+    }
   }
   
-  
-  
-  
+  date <- format(time, "%Y%m%d")
+  time <- format(time, "%H%M")  
   
 }
